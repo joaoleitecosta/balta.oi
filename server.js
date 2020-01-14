497d223e82f13083ac1c29d0d65422cb5e42fe36
Joao Leite <joao_leite@hotmail.com.br>
2020-01-13 22:00:44 -0300

    starting debug

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use("/", route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 console.log("API rodando na porta", port);
 
 function normalizePort(val) {
@@ -60,4 +61,13 @@ function onError(error) {
         default:
           throw error;    
       }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+       ? 'pipe ' + addr 
+       : 'port ' + addr.port;
+
+       debug('Listening on ' + bind);
 }
